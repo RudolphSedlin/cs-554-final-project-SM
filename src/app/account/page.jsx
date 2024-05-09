e897f6e9617bb9fa112fbd884445990a8ee36e53
OverkillLC <97358665+OverkillLC@users.noreply.github.com>
2024-05-09 00:23:45 +0000

    testing account page

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -5,23 +5,49 @@ import {AuthProvider, AuthContext, useAuthContext} from '@/context/AuthContext';
 
 
 
-export default function account() {
-  let {user} = useAuthContext();
-  console.log(user);
-return (
-    <div>
-        <ul className='navClass center'>
-            <li className='nav'>
-                <Link href={'/register'} hidden = {user}>Register</Link>
-            </li>
-            <li className='nav'>
-                <Link href={'/login'}hidden = {user}>Login</Link>
-            </li>
-            <li className='nav'>
-                <Link href={'/logout'} hidden = {!user}>Logout</Link>
-            </li>
-        </ul>
-    </div>
-
-);
+export default async function account() {
+    let {user} = useAuthContext();
+    console.log(user);
+    let userInfo = <div></div>;
+    if (user != null) {
+        try {
+            const response = await fetch('/api/account', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: user.displayName
+                })
+            });
+            const data = await response.json();
+            if (data.success) {
+                const dbUser = data.user;
+                userInfo = <div>
+                    <h1>{dbUser.username}'s Profile</h1>
+                    <h2>Full Name: {dbUser.name.first} {dbUser.name.last}</h2>
+                    <p>{dbUser.bio}</p>
+                </div>;
+            } else {
+                console.error('Failed to fetch user:', data.message);
+            }
+        } catch (error) {
+            console.error('Error submitting form:', error);
+        }
+    }
+    return (
+        <div>
+            <ul className='navClass center'>
+                <li className='nav'>
+                    <Link href={'/register'} hidden = {user}>Register</Link>
+                </li>
+                <li className='nav'>
+                    <Link href={'/login'}hidden = {user}>Login</Link>
+                </li>
+                <li className='nav'>
+                    <Link href={'/logout'} hidden = {!user}>Logout</Link>
+                </li>
+            </ul>
+        </div>
+    );
 }
